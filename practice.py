f08e13af482cf0b6d85a6607c3252d57af8c9a32
Andrew Zaiko <andrewzaiko@gmail.com>
2022-07-13 12:54:08 +0300

    Конечная версия

--- practice.py
+++ practice.py
@@ -1,3 +1,15 @@
-from flask import Flask, request, render_template
+from flask import Flask
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def main_page():
+    page_content = """
+    <h1>Смотри, это - моя страничка</h1>
+    <p>Страничек много, но эта - моя!</p>
+    <p>Фронтенд - мой лучший друг!</p>
+    """
+    return page_content
+
+app.run()
